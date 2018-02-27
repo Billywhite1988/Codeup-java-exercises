0bd8b8b81e9a7a3c7b944d25b652ca4f1583ef46
Billy <billy_white.88@yahoo.com>
2018-02-27 17:00:16 -0600

    ControlFlowEX homework

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,10 +2,25 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i < 10) {
-            System.out.println("i is " + i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+
+//        int i = 0;
+//            do {
+//                System.out.println(i);
+//                i += 2;
+//            } while(i <= 100);
+
+//        Alter your loop to count backwards by 5's from 100 to -10.
+
+            int i = 100;
+            do {
+                System.out.println(i);
+                i -= 5;
+            } while(i >= -10);
     }
 }
+
