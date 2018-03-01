2383a82e0d06a069a1222eccf720a97b45676e22
Billy <billy_white.88@yahoo.com>
2018-03-01 10:05:02 -0600

    "Created Bob question"

--- src/Bob.java
+++ src/Bob.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 
     public class Bob {
 
+        public static void main(String[] args) {
+            System.out.println("Say something to Bob!");
+            Scanner scan = new Scanner(System.in);
 
+            String userInput = scan.nextLine();
+
+            // Bob answers 'Sure.' if you ask him a question.
+            boolean endsWithQuestionMark = userInput.endsWith("?");
+
+            // Bob answers "Whoa, chill out!" if you yell (Yelling is all uppercase or ending with a ! character
+            boolean isYelling = userInput.equals(userInput.toUpperCase()) || userInput.endsWith("!");
+
+            // Bob says "Fine. Be that way" if you don't actually say anything
+            boolean didNotSayAnything = userInput.trim().isEmpty();
 
 
     }
