3d608414bfda1c4b1764d7a8c00ca36942f585cd
Billy <billy_white.88@yahoo.com>
2018-03-01 11:17:00 -0600

    Completed Q1 and Q2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,14 +1,24 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
+
     public static void main(String[] args) {
 
-        // Testing Exercise 1
-        System.out.println(add(2.5, 2.5));
-        System.out.println(subtract(2, 2));
-        System.out.println(multiply(2, 2));
-        System.out.println(divide(2, 10));
-        System.out.println(modulus(100, 2));
+/////////// 1. Create four separate methods. Each will perform an arithmetic operation: /////////////
+            System.out.println(add(2.5, 2.5));
+            System.out.println(subtract(2, 2));
+            System.out.println(multiply(2, 2));
+            System.out.println(divide(2, 10));
+            System.out.println(modulus(100, 2));
+
+/////////// 2. Create a method that validates that user input is in a certain range ////////////////
+//            System.out.print("Enter a number between 1 and 10: ");
+//            int userInput = getInteger(1, 10);
+//            System.out.println("Your integer is: " + userInput);
 
     }
+
+////////// 1. Create four separate methods. Each will perform an arithmetic operation: /////////////
 
     public static double add(double num1, double num2) {
         return num1 + num2;
@@ -25,4 +35,23 @@ public class MethodsExercises {
     public static double modulus(double num1, double num2) {
         return num1 % num2;
     }
+
+
+//////// 2. Create a method that validates that user input is in a certain range ////////////////
+
+//    public static int getInteger(int min, int max) {
+//        Scanner sc = new Scanner(System.in);
+//        if (!sc.hasNextInt()) {
+//            System.out.println("Not a number!");
+//            return getInteger(min, max);
+//        }
+//        int userInput = sc.nextInt();
+//        if (userInput >= min && userInput <= max) {
+//            return userInput;
+//        } else {
+//            System.out.println("Number not in range!");
+//            return getInteger(min, max);
+//        }
+//    }
+
 }
