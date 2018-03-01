cf8ebeb00df3974c8c0400d642714dcc78ce80cc
Billy <billy_white.88@yahoo.com>
2018-03-01 11:55:38 -0600

    Completed Q4

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -19,7 +19,11 @@ public class MethodsExercises {
 /////////// 3. Calculate the factorial of a number. ////////////////////////////////////////////////
 
             Scanner sc = new Scanner(System.in);
-            userInteractionFactorial(sc);
+//            userInteractionFactorial(sc);
+
+/////////// 4. Create an application that simulates dice rolling ///////////////////////////////////
+
+            rollDiceGame(sc);
 
 
     }
@@ -64,29 +68,52 @@ public class MethodsExercises {
 
 /////////// 3. Calculate the factorial of a number. ////////////////////////////////////////////////
 //
-    public static long calculateFactorial(int num) {
-        int output = 1;
-        for (int i = 1; i <= num; i += 1) {
-            output *= i;
-        }
-        return output;
+//    public static long calculateFactorial(int num) {
+//        int output = 1;
+//        for (int i = 1; i <= num; i += 1) {
+//            output *= i;
+//        }
+//        return output;
+//    }
+//
+//    public static void userInteractionFactorial(Scanner sc) {
+//        boolean willContinue;
+//        String userChoice;
+//        do {
+//            System.out.println("Please enter an integer from 1 to 12");
+//            int userInt = getInteger(1, 12);
+//            System.out.println(calculateFactorial(userInt));
+//            do {
+//                System.out.println("Do you wish to continue? [y/n]: ");
+//                userChoice = sc.next().trim();
+//            } while (!userChoice.equalsIgnoreCase("y") & !userChoice.equalsIgnoreCase("n"));
+//
+//            willContinue = userChoice.equalsIgnoreCase("y");
+//        } while (willContinue);
+//    }
+//
+//
+//
+//////// 4. Create an application that simulates dice rolling ///////////////////////////////////
+
+    public static int randomWithRange(int min, int max)
+    {
+        int range = (max - min) + 1;
+        return (int)(Math.random() * range) + min;
     }
 
-    public static void userInteractionFactorial(Scanner sc) {
-        boolean willContinue;
+    public static void rollDiceGame(Scanner sc) {
         String userChoice;
+        System.out.print("Please enter the number of sides for the dice: ");
+        int numberSides = getInteger(1, 20);
         do {
-            System.out.println("Please enter an integer from 1 to 12");
-            int userInt = getInteger(1, 12);
-            System.out.println(calculateFactorial(userInt));
+            System.out.println("You have rolled a " + randomWithRange(1, numberSides) + " and a " + randomWithRange(1, numberSides));
             do {
-                System.out.println("Do you wish to continue? [y/n]: ");
+                System.out.println("Would you like to roll again? [y/n]");
                 userChoice = sc.next().trim();
             } while (!userChoice.equalsIgnoreCase("y") & !userChoice.equalsIgnoreCase("n"));
 
-            willContinue = userChoice.equalsIgnoreCase("y");
-        } while (willContinue);
+        } while (!userChoice.equals("n"));
     }
 
-    }
-
+}
