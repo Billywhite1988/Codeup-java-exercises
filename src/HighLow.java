40d6aa9d08f70f87d0d22c8ffb0a12089801948c
Billy <billy_white.88@yahoo.com>
2018-03-01 13:54:19 -0600

    Completed the foundation,just need to add math.round and integer to generate number if number in range

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,3 +1,51 @@
+import java.util.Scanner;
+
 public class HighLow {
+
+    public static int numberOfGuesses;
+    public static int gameNumber;
+
+    public static void main(String[] args) {
+        initGame();
+    }
+
+    public static void initGame() {
+        boolean programRunning;
+        boolean gameRunning = true;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the High / Low Guessing Game!");
+        do {
+            gameNumber = randomWithRange(1, 100);
+            System.out.println("The random number is: " + gameNumber);
+            do {
+                gameRunning = runRound(sc);
+            } while (gameRunning);
+            programRunning = getYesOrNo(sc).equals("y");
+        } while (programRunning);
+        System.out.println("Good bye!");
+    }
+
+    public static boolean runRound(Scanner sc) {
+        boolean gameNotWon = false;
+        System.out.print("Please enter a guess: ");
+        int playerGuess = getInteger(1, 100, sc);
+        if (playerGuess == gameNumber) {
+            System.out.println("GOOD GUESS!");
+            gameNotWon = false;
+        } else if (playerGuess > gameNumber) {
+            System.out.println("LOWER");
+            numberOfGuesses++;
+            gameNotWon = true;
+            System.out.println("Number of guesses made: " + numberOfGuesses);
+        } else {
+            System.out.println("HIGHER");
+            gameNotWon = true;
+            numberOfGuesses++;
+            System.out.println("Number of guesses made: " + numberOfGuesses);
+        }
+        return gameNotWon;
+    }
+
+
 
 }
