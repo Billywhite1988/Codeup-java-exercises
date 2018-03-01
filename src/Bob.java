c660de7ebbde305c63f465b1e6a547ab2a22e47a
Billy <billy_white.88@yahoo.com>
2018-03-01 09:49:50 -0600

    "first commit"

---
+++ src/Bob.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+//    Create a class Bob for the following exercise with a main method. Bob is a lackadaisical teenager.In conversation, his responses are very limited.
+
+    public class Bob {
+
+
+
+
+    }
+
