e1d17601bb5d746a889d26ff9cb53fd43eb7edf5
Billy <billy_white.88@yahoo.com>
2018-03-01 09:22:54 -0600

    "Q1 and Q2 completed"

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,3 +1,23 @@
 public class StringExercise {
 
+    public static void main(String[] args) {
+        String message;
+
+        // exercise 1
+        //    We don't need no education
+        //    We don't need no thought control
+        message = "We don't need no education\n"
+                + "We don't need no thought control"
+                + "\n ------ \n"
+
+                // exercise 2
+                // Check "this" out!, "s inside of "s!
+                + "Check \"this\" out!, \"s inside of \"s!"
+                + "\n ------ \n";
+
+
+
+        System.out.println(message);
+    }
 }
+
