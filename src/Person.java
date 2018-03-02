fba483c0438cc11ac41dfe48b90d8af93785faeb
Billy <billy_white.88@yahoo.com>
2018-03-02 15:43:58 -0600

    first commit

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    // Change to private
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+    // Getter get the property
+    public String getName() {
+        return name;
+    }
+
+    // Setter set the value of the object
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
