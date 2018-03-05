568b0f531e3adc51794030e3e451ed040fa0ef69
Billy <billy_white.88@yahoo.com>
2018-03-05 11:29:39 -0600

    create a server name generator ultizing strings and math.random.

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -3,12 +3,22 @@
 //   Add a main method, and inside of your main method select and random noun and adjective and
 //   hyphenate the combination and display the generated name to the user
 
-public class ServerNameGenerator {
+    public class ServerNameGenerator {
     public static String getRandomString(String[] strings) {
-        int randomIndex = (int) (Math.random() * strings.length);
-        return strings[randomIndex];
+        int randomElement = (int) (Math.random() * strings.length);
+        return strings[randomElement];
     }
 
+    public static void main (String [] args) {
+
+    String[] nouns = {"business", "child", "day", "hand", "home", "life", "job", "people", "government", "company"};
+
+    String[] adjectives = {"attractive", "beautiful", "thoughtful", "fancy", "gorgeous", "muscular", "random", "handsome", "skinny", "chubby"};
+
+    String serverName = getRandomString(adjectives) + "-" + getRandomString(nouns);
+
+        System.out.println("Your server name is: " + serverName);
 
 
+    }
 }
