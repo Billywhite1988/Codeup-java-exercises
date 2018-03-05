588a29facb115db28e6f5393df04ba5478e691fe
Billy <billy_white.88@yahoo.com>
2018-03-05 12:14:00 -0600

    creating movie application

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,4 +1,30 @@
 package movies;
 
+import util.Input;
+
 public class MoviesApplication {
+    public static void main(String[] args) {
+        Input input = new Input();
+        String userChoice;
+
+        System.out.println("Welcome to MovieLister!");
+        System.out.println("What would you like to do?");
+
+        do {
+            showMenu();
+            System.out.println("Enter your choice: ");
+            userChoice = input.getString();
+
+            if (userChoice.equals("1")) {
+                showAll();
+
+            } else if (userChoice.equals("2")) {
+                showMoviesByCategory("animated");
+
+            } else if (userChoice.equals("3")) {
+                showMoviesByCategory("drama");
+            }
+
+        }
+    }
 }
