93d44c3b14e7729a6cd91ed7783a9b7b6f33ae14
Billy <billy_white.88@yahoo.com>
2018-03-08 15:34:42 -0600

    Grade Application

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,72 @@
+package grades;
+
+import util.Input;
+import java.util.HashMap;
+
+public class GradesApplication {
+    public static void main(String[] args) {
+        Input input = new Input();
+        HashMap<String, Student> students = new HashMap<>();
+        String userInput;
+
+        System.out.println("Welcome to the Grade Application");
+        students = addGithubGrades();
+
+        for(String key : students.keySet()) {
+            System.out.print(key + "  |  ");
+        }
+
+        System.out.println();
+        System.out.println("--------------------------------------------");
+
+        do {
+
+            System.out.println("Here are the github usernames of our students");
+            userInput = input.getString();
+
+            if(students.containsKey(userInput)) {
+                Student student = students.get(userInput);
+                System.out.println(student.getName() + "\n Has a grade average of " + student.getGradeAverage());
+            } else {
+                System.out.println("Sorry, no student found with the github username of");
+            }
+
+        } while (input.yesNo(true));
+
+    }
+
+    public static HashMap<String, Student> addGithubGrades() {
+        HashMap<String, Student> students = new HashMap<>();
+
+        Student student1 = new Student("Name: RayRay - Github Username: Lil-Ray14");
+        student1.addGrade(100);
+        student1.addGrade(95);
+        student1.addGrade(80);
+        student1.addGrade(99);
+        student1.addGrade(95);
+
+
+
+        Student student2 = new Student("Name: Angela - Github Username: Angela-Reina08");
+        student2.addGrade(85);
+        student2.addGrade(80);
+        student2.addGrade(75);
+        student2.addGrade(70);
+        student2.addGrade(65);
+
+        Student student3 = new Student("Name: Amber - Github Username: Amber-Nicole96");
+        student3.addGrade(60);
+        student3.addGrade(65);
+        student3.addGrade(55);
+        student3.addGrade(50);
+        student3.addGrade(45);
+
+
+        students.put("Lil Ray14", student1);
+        students.put("Angela Reina08", student2);
+        students.put("Amber Nicole96", student3);
+
+
+        return students;
+    }
+}
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -5,15 +5,19 @@ public class Student {
 
     private String name;
 
-    //    declaring a arraylist
+//    declaring a arraylist
     private ArrayList<Integer> grades;
 
-    //    constructor that takes in a name argument and uses it to set the name
+//    constructor that takes in a name argument and uses it to set the name
     public Student(String name) {
         this.name = name;
 
 //    initializing grades
         grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
     }
 
     public double getGradeAverage() {
@@ -22,13 +26,36 @@ public class Student {
         double average;
 
 
-        for (int grade : grades) {
+        for(int grade : grades) {
             sumOfAllGrades += grade;
         }
 
-        average = sumOfAllGrades / numberOfgrades;
+        average  = sumOfAllGrades / numberOfgrades;
+        return average;
 
     }
 
+    public void addGrade(int grade) {
+        grades.add(grade);
+
 }
 
+//
+//// Write just enough code in the main method to test the class's methods before using this class elsewhere...
+public static void main(String[] args) {
+        System.out.println("Testing the Student class");
+        Student student = new Student("Angela");
+        System.out.println("The number of elements in the grades array is " + student.grades.size() + "... Great! It work");
+        System.out.println("Display three grades, a 100, a 90, and an 80");
+        student.addGrade(100);
+        student.addGrade(90);
+        student.addGrade(80);
+        double expectedGrade = (100 + 90 + 80) / 3;
+        System.out.println("The average is " + expectedGrade);
+
+
+        }
+
+
+
+}
