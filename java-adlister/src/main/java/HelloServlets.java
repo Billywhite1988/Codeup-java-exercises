0f77e3bcfa9072f337d657785f0f7d0b8ace30a5
Billy <billy_white.88@yahoo.com>
2018-03-26 15:48:04 -0500

    Added counter to display number of page count

--- java-adlister/src/main/java/HelloServlets.java
+++ java-adlister/src/main/java/HelloServlets.java
@@ -3,10 +3,26 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 @WebServlet(name = "HelloServlet", urlPatterns = "/hello")
-public class HelloServlets extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+    public class HelloServlets extends HttpServlet {
+        private int counter = 0;
+
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.setContentType("text/html");
+
+
+            PrintWriter out = response.getWriter();
+
+            String name = request.getParameter("name");
+            if (name != null) {
+                response.getWriter().println("<h1>Hello, " + name + "</h1>");
+            } else {
+                out.println("<h1>Hello World</h1>");
+            }
+            // you could also getServletName() in place of name and delete String name = request getParameter("name")
+            counter += 1;
+            response.getWriter().println("<h3>Number of page visits: " + counter + ".</h3>");
+
+
+
+        }
     }
-}
