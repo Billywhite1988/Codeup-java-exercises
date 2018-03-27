866959aef7c3f90eeab90f9eae6cb8dab07bff5d
Billy <billy_white.88@yahoo.com>
2018-03-27 11:57:44 -0500

    Update hello world servlet

---
+++ helloworldservlet/src/main/java/helloworldservlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello/*")
+public class helloworldservlet extends HttpServlet {
+
+    private int viewCounter;
+
+    public void init() {
+        viewCounter = 0;
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+        try {
+            if(req.getParameter("reset") != null)
+                viewCounter = 0;
+            viewCounter++;
+            res.setContentType("text/html");
+            PrintWriter out = res.getWriter();
+            out.println(String.format("<h1>Hello %s!</h1><h2>%d Page Views!</h2>", req.getParameter("name"), viewCounter));
+        } catch (IOException e) {
+            System.out.println("Darn.");
+        }
+    }
+}
